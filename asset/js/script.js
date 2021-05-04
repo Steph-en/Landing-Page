cebfa014fa9abe27602618e1597e1d87490c9d24
Steph-en <appahstephen9@gmail.com>
2021-05-04 11:52:22 +0000

    deleted all the code in htlm and css starting over

--- asset/js/script.js
+++ asset/js/script.js
@@ -8,10 +8,15 @@ var header = document.getElementById("myHeader");
 var sticky = header.offsetTop;
 
 // Add the sticky class to the header when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.pageYOffset > sticky) {
+function myFunction() 
+{
+  if (window.pageYOffset > sticky) 
+  {
     header.classList.add("sticky");
-  } else {
+  } 
+  
+  else 
+  {
     header.classList.remove("sticky");
   }
 }
